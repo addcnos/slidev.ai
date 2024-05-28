c52b2d1abe1921bfe6fa8ee68f360d74405ccd31
zhangmanyi <zhangmanyi@addcn.com>
2024-05-28 16:50:38 +0800

    feat: local packager

--- electron/slidev.server.ts
+++ electron/slidev.server.ts
@@ -2,10 +2,31 @@ import path from 'path';
 import fs from 'fs-extra';
 import { exec } from 'child_process';
 import { app } from 'electron';
-// import os from 'os';
 import pkg from '../package.json'
-// const TEMP_DIR = path.join(os.tmpdir(), 'slidev-local-service');
 const TEMP_DIR = path.join(app.getPath('userData'), 'slidev-local-service');
+const LOG_FILE_PATH = path.join(app.getPath('userData'), 'logs');
+
+// 创建日志文件
+const writeLog = (message:string) => {
+  const logFilePath = path.join(LOG_FILE_PATH, 'app.log')
+  // 确保日志目录存在
+  if (!fs.existsSync(LOG_FILE_PATH)) {
+    fs.mkdirSync(LOG_FILE_PATH);
+  }
+  // 写入日志信息
+  const logMessage = `${new Date().toISOString()} - ${message}\n`;
+  fs.appendFileSync(logFilePath, logMessage, 'utf8');
+}
+
+// 测试写一个30秒更新一次slides.md的内容
+const updateSlides = () => {
+  const slidesPath = path.join(TEMP_DIR, 'slides.md');
+  let count = 0;
+  setInterval(() => {
+    count++;
+    fs.writeFileSync(slidesPath, `# Slide ${count}\n\nThis is slide ${count}`);
+  }, 10000);
+}
 
 // 构建一个package.json文件，用于安装Slidev
 const createPackageJson = async () => {
@@ -38,30 +59,29 @@ const copyFils = async () => {
   try {
     await createPackageJson();
     await fs.copy(resourcesPath, TEMP_DIR, { overwrite: true });
-    console.log('拷贝完成');
+    writeLog('拷贝完成');
   } catch (err) {
-    console.error('拷贝过程中发生错误:', err);
+    writeLog('拷贝过程中发生错误'+JSON.stringify(err));
   }
 }
-
 
 const startSlidev = () => {
   // 启动 Slidev 服务
   const slidevProcess = exec(`npx slidev`, { cwd: TEMP_DIR });
 
   slidevProcess.stdout.on('data', (data) => {
-    console.log(`Slidev: ${data}`);
+    writeLog(`Slidev: ${data}`);
   });
 
   slidevProcess.stderr.on('data', (data) => {
-    console.error(`Slidev error: ${data}`);
+    writeLog(`Slidev error: ${data}`);
   });
 
   slidevProcess.on('close', (code) => {
-    console.log(`Slidev process exited with code ${code}`);
+    writeLog(`Slidev process exited with code ${code}`);
   });
+  updateSlides();
 }
-
 
 export const createSlidevServer = async () => {
   // 创建临时目录
@@ -71,19 +91,19 @@ export const createSlidevServer = async () => {
   // 读取临时目录内的版本信息，如果版本信息不一致，删除临时目录，重新下载，如果一致，直接启动 Slidev 服务
   // HACK 由于 Slidev 服务启动后会占用端口，导致无法重新启动，所以这里每次启动服务前都会删除临时目录
   // HACK 考虑版本检查，如果版本不一致，删除临时目录，重新下载
-  console.log(getResourcesPath());
-  
   if (fs.existsSync(path.resolve(TEMP_DIR, 'node_modules/@slidev/cli'))) {
-    console.log('Slidev already installed.');
+    writeLog('Slidev already installed.');
     startSlidev();
   } else {
     await copyFils()
-    exec('npm install', { cwd: TEMP_DIR }, (err, stdout, stderr) => {
+    const yarnPath = path.join(TEMP_DIR, 'scripts', 'yarn.cjs');
+    
+    exec(`node "${yarnPath}" install`, { cwd: TEMP_DIR, env: process.env}, (err, stdout, stderr) => {
       if (err) {
-        console.error(`Error installing Slidev: ${stderr}`);
+        writeLog(`Error installing Slidev: ${stderr}`);
         return;
       }
-      console.log('Slidev installed successfully.');
+      writeLog('Slidev installed successfully.');
       startSlidev();
     });
   }
