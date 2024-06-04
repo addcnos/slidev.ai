765775ecdaeb6d44b66e0bf455e22e4b98ad2cc6
brokenbonesdd <brokenbonesdd@gmail.com>
2024-06-04 18:48:51 +0800

    chore: Update useOutlineStore to set visible to false

--- src/store/useOutlineStore.ts
+++ src/store/useOutlineStore.ts
@@ -3,7 +3,7 @@ import { createSharedComposable } from "@vueuse/core";
 
 
 export const useOutlineStore = createSharedComposable(() => {
-  const visible = ref<boolean>(true)
+  const visible = ref<boolean>(false)
   const theme = ref<string>('如何学习日语') // 大纲标题
   const count = ref<number>(10) // 大纲数量
 
