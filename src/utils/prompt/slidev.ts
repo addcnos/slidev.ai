7574045ad6ae4acc70dda51ea61458b25d0d0dfd
ProsperBao <baihe2233@gmail.com>
2024-06-28 17:33:53 +0800

    chore: Update image element with new class for positioning

--- src/utils/prompt/slidev.ts
+++ src/utils/prompt/slidev.ts
@@ -49,7 +49,7 @@ export function beautifySlidevPrompt(process: string, message: string, content: 
     '',
     '这里是我的要求:',
     message,
-    '注意：图片的输出格式为 <img v-drag src="" />',
+    '注意：如果生成了图片的输出格式为 <img v-drag src="" />',
     '',
     '当前的内容是:',
     content
@@ -62,7 +62,7 @@ export function expendSlidevPrompt(_: string, message: string, content: string) 
     '',
     '这里是我的要求:',
     message,
-    '注意：图片的输出格式为 <img v-drag src="" />',
+    '注意：如果生成了图片的输出格式为 <img v-drag src="" />',
     '',
     '当前的内容是:',
     content
@@ -75,7 +75,7 @@ export function polishSlidevPrompt(process: string, message: string, content: st
     '',
     '这里是我的要求:',
     message,
-    '注意：图片的输出格式为 <img v-drag src="" />',
+    '注意：如果生成了图片的输出格式为 <img v-drag src="" />',
     '',
     '当前的内容是:',
     content
